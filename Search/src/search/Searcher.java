cff32713c0ef5e476c80e09b8d4f6b014faae67c
Hayley Mapley <40159686+hayleymapley@users.noreply.github.com>
2018-10-08 12:44:18 +1300

    Update to Searcher class

--- Search/src/search/Searcher.java
+++ Search/src/search/Searcher.java
@@ -3,10 +3,10 @@ package search;
 public interface Searcher {
 
 	//Search used by linearSearcher, requiring array and key value
-	public abstract int makeSearch(int[] array, int key);
+	public int makeSearch(int[] array, int key);
 
 	//Search used by BinarySearcher, requiring array and key value, with possibility
 	//to edit the bounds that you wish to search
-	public abstract int makeSearch(int[] array, int left, int right, int key);
+	public int makeSearch(int[] array, int left, int right, int key);
 	
 }
